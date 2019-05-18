aff8842842173a2540ee633c3affd1402965bbd0
Apple <>
2019-05-17 22:12:54 -0800

    Add iOS/tvOS simulator support.  Minor cleanup and fixes

--- Application/main.m
+++ Application/main.m
@@ -17,8 +17,8 @@ Application entry point for all platforms.
 
 int main(int argc, char * argv[]) {
 
-#if TARGET_OS_SIMULATOR
-#error No simulator support for Metal API.  Must build for a device
+#if TARGET_OS_SIMULATOR && (!defined(__IPHONE_13_0) ||  !defined(__TVOS_13_0))
+#error No simulator support for Metal API for this SDK version.  Must build for a device
 #endif
 
     @autoreleasepool {
